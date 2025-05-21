ad6b157c601cc3e6a900ab9ef95003048686b4ba
kes <alexismanosca@gmail.com>
2025-05-21 21:58:33 +1200

    Added a few more debugging and cleaned up the code
    Since it uses UDP, it doesn't always send the packets in full so just be aware of that.

--- Client.java
+++ Client.java
@@ -49,6 +49,12 @@ public class Client {
 
                 // Check the status of the request
                 if(responseArray[0].equals("OK")){
+                    // Ensure we are at the right file
+                    if (!responseArray[1].equals(file)){
+                        System.out.println("Error: file name doesn't match");
+                        continue;
+                    }
+                    
                     // Find the file size and data port
                     int fileSize = Integer.parseInt(responseArray[3]);
                     int portNumber = Integer.parseInt(responseArray[5]);
@@ -56,11 +62,9 @@ public class Client {
                     // Create a new random access file to write the file
                     RandomAccessFile raf = new RandomAccessFile("Client_Files/" + file, "rw");
 
-                    // List to keep track of the received bytes
+                    // List to keep track of the received bytes and requested bytes
                     ArrayList<int[]> receivedBytes = new ArrayList<int[]>();
-
-                    // Keep track of how many requests have been sent
-                    int requestsSent = 0;
+                    ArrayList<int[]> requestedBytes = new ArrayList<int[]>();
 
                     // Send to the server the request on the new port
                     for (int i = 0; i < fileSize; i += 1000) {
@@ -73,56 +77,84 @@ public class Client {
                         DatagramPacket dataPacket = new DatagramPacket(dataBytes, dataBytes.length, serverAddress, portNumber);
                         clientSocket.send(dataPacket);
 
-                        // Increment the requests sent
-                        requestsSent++;
+                        // Add the range to requested bytes
+                        requestedBytes.add(new int[]{i, endByte});
                     }
 
+                    // Keep count on how many times the client retries to request a missing packet
+                    int retries = 0;
+
                     // Wait for responses from the server
-                    while (receivedBytes.size() < requestsSent){
-                        // Receive data from the server
-                        byte[] downloadedData = new byte[2048];
-                        DatagramPacket downloadedPacket = new DatagramPacket(downloadedData, downloadedData.length);
-                        clientSocket.receive(downloadedPacket);
-                        String dataResponse = new String(downloadedPacket.getData(), 0, downloadedPacket.getLength());
-                        System.out.println("Server response: " + dataResponse);
+                    while (!requestedBytes.isEmpty() && retries < 5){
+                        try{
+                            // Timeout after 2 seconds
+                            clientSocket.setSoTimeout(2000); 
 
-                        // Split the get the data from the response
-                        String[] dataResponseArray = dataResponse.split(" ");
+                            // Receive data from the server
+                            byte[] downloadedData = new byte[2048];
+                            DatagramPacket downloadedPacket = new DatagramPacket(downloadedData, downloadedData.length);
+                            clientSocket.receive(downloadedPacket);
+                            String dataResponse = new String(downloadedPacket.getData(), 0, downloadedPacket.getLength());
+                            System.out.println("Server response: " + dataResponse);
 
-                        // Grab the start and end bytes
-                        int startByte = Integer.parseInt(dataResponseArray[4]);
-                        int endByte = Integer.parseInt(dataResponseArray[6]);
+                            // Split the get the data from the response
+                            String[] dataResponseArray = dataResponse.split(" ");
 
-                        // Check if the start and end bytes have already been written
-                        boolean isDuplicate = false;
-                        for (int[] range : receivedBytes) {
-                            if (Arrays.equals(range, new int[]{startByte, endByte})){
-                                isDuplicate = true;
-                                break;
+                            // Grab the start and end bytes
+                            int startByte = Integer.parseInt(dataResponseArray[4]);
+                            int endByte = Integer.parseInt(dataResponseArray[6]);
+
+                            // Check if the start and end bytes have already been written
+                            boolean isDuplicate = false;
+                            for (int[] range : receivedBytes) {
+                                if (Arrays.equals(range, new int[]{startByte, endByte})){
+                                    isDuplicate = true;
+                                    break;
+                                }
                             }
+
+                            // If duplicate has been found then continue
+                            if (isDuplicate){
+                                continue;
+                            }
+
+                            // Find where the word DATA is
+                            int dataIndex = dataResponse.indexOf("DATA ");
+
+                            // Decode the data to bytes
+                            String base64Data = dataResponse.substring(dataIndex + 5).trim();
+                            byte[] rangeByte = Base64.getDecoder().decode(base64Data);
+
+                            // Write to the random access file
+                            raf.seek(startByte);
+                            raf.write(rangeByte);
+
+                            // Keep track of the bytes that have been written already and remove it from requested bytes
+                            receivedBytes.add(new int[]{startByte, endByte});
+                            int removeIndex = 0;
+                            for (int[] range : requestedBytes) {
+                                if (range[0] == startByte && range[1] == endByte)
+                                    removeIndex = requestedBytes.indexOf(range);
+                                    break;
+                            }
+                            requestedBytes.remove(removeIndex);
                         }
+                        // If timeout occurs
+                        catch (SocketTimeoutException e){
+                            // Loop through the requested list and re-request them
+                            for (int[] range : requestedBytes) {
+                                // Send request
+                                String dataRequest = "FILE " + file + " GET START " + range[0] + " END " + range[1];
+                                byte[] dataBytes = dataRequest.getBytes();
+                                DatagramPacket dataPacket = new DatagramPacket(dataBytes, dataBytes.length, serverAddress, portNumber);
+                                clientSocket.send(dataPacket);
+                            }
 
-                        // If duplicate has been found then continue
-                        if (isDuplicate){
-                            continue;
+                            // Increment the number of retries
+                            retries++;
+
                         }
-
-                        // Find where the word DATA is
-                        int dataIndex = dataResponse.indexOf("DATA ");
-
-                        // Decode the data to bytes
-                        String base64Data = dataResponse.substring(dataIndex + 5).trim();
-                        byte[] rangeByte = Base64.getDecoder().decode(base64Data);
-
-                        // Write to the random access file
-                        raf.seek(startByte);
-                        raf.write(rangeByte);
-
-                        System.out.println("Writing bytes from " + startByte + " to " + endByte);
-                        System.out.println("Decoded byte length: " + rangeByte.length);
-
-                        // Keep track of the bytes that have been written already
-                        receivedBytes.add(new int[]{startByte, endByte});
+                        
                     }
 
                     // Close the file
